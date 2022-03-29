6c82f1e17b7f22175c742bc4fb65f118ffe70cf7
W0iD <xcrypter@gmail.com>
2022-03-30 00:00:44 +0300

    homework4

--- src/main/java/ru/gb/chat/ChatController.java
+++ src/main/java/ru/gb/chat/ChatController.java
@@ -1,8 +1,33 @@
 package ru.gb.chat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class ChatController {
+    @FXML
+    public TextArea messageArea;
 
+    @FXML
+    public TextField messageField;
+
+    private final Alert alert = new Alert(Alert.AlertType.WARNING,"Введите текст сообщения", ButtonType.OK);
+
+    @FXML
+    protected void sendMessage(ActionEvent actionEvent) {
+        if (!(messageField.getText().isEmpty())) {
+            messageArea.appendText(messageField.getText() + "\n");
+            messageField.clear();
+            messageField.requestFocus();
+        }else{
+            alert.showAndWait();
+        }
+    }
+
+    public void exitChat(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
