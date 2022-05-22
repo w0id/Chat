d2404ec081c6594c52dc8117c46da4ae750f2ffc
W0iD <xcrypter@gmail.com>
2022-05-22 18:52:55 +0300

    homework6

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ru.gb.chat {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.apache.logging.log4j;
 
     exports ru.gb.chat.client;
     opens ru.gb.chat.client to javafx.fxml;
--- src/main/java/ru/gb/chat/server/MessageHistory.java
+++ src/main/java/ru/gb/chat/server/MessageHistory.java
@@ -1,16 +1,21 @@
 package ru.gb.chat.server;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class MessageHistory {
+    private static final Logger log = LogManager.getLogger(ChatServer.class);
 
     public void write(String nick, String message) {
         try (final FileOutputStream out = new FileOutputStream(nick + ".txt", true)) {
             byte[] bytes = (message + "\n").getBytes(StandardCharsets.UTF_8);
             out.write(bytes);
         } catch (IOException e) {
+            log.error("error occured: {}", e);
             throw new RuntimeException(e);
         }
     }
